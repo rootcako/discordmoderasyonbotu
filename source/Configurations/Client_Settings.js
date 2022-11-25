11843ab5f5c5c3c94c1cfb9b13771e999c0eff63
rootcako <118899237+rootcako@users.noreply.github.com>
2022-11-25 19:21:35 +0400

    Update Client_Settings.js

--- source/Configurations/Client_Settings.js
+++ source/Configurations/Client_Settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-    botToken: "MTAzNjAxOTk0MjUzNDkzODYyNA.GOtYLk.5r66TwJ5z_mlrBP7PXnfR7MWKtoReo8D1tIzec",
-    clientOwner: ["970411727885848608"],
+    botToken: "",
+    clientOwner: [""],
     prefix: ["."],
 
     voiceChannel: "1036813801233858661",
